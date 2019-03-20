c48d7f033732675d07a04381e92cf8046ad5b926
SOYEONG OH <axionoso@gmail.com>
2019-03-20 16:38:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls (array)
 end
 
 def find_the_cheese (array)
-  array.include?("cheddar")
+  array.include?("cheddar" || "gouda" || "camembert")
 
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
